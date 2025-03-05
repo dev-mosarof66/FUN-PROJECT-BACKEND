908f2d18e025c66ddd02d79ec113651ba47b2b68
dev-mosarof066 <mdmosarofhossain066@gmail.com>
2025-03-05 13:08:47 +0600

    DAY 02

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-app.use(cors());
 
 //import routers
 
